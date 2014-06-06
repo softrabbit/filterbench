f6104539820406196a04b869965b0307f0c0001f
Raine M. Ekman <raine@iki.fi>
2014-06-06 15:52:08 +0300

    - Added denormal and coefficient benchmarks - Smartified filterdriver to handle filter selection instead of shell

--- bench.sh
+++ bench.sh
@@ -4,52 +4,59 @@
 FILTERS="LowPass HiPass BandPass_CSG BandPass_CZPG Notch AllPass Moog DoubleLowPass Lowpass_RC12 Bandpass_RC12	Highpass_RC12 Lowpass_RC24 Bandpass_RC24 Highpass_RC24 Formantfilter"
 # "faster" filters, the ones not 4x oversampled internally
 #FILTERS="LowPass HiPass BandPass_CSG BandPass_CZPG Notch AllPass"
-FILTERS="Lowpass_RC24"
+#FILTERS="Formantfilter"
+
+# How to optimize
+OPTIMIZE="-O3 -msse2 -ftree-vectorize -mfpmath=sse"
 
 BINDIR=tests
 OUTDIR=output
 
 if [ $# -eq 0 ] ; then
     echo "Usage: $0 [options]"
-    echo -e "\t--compile\tBuild all versions"
-    echo -e "\t--mod    \tBuild modified version"
-    echo -e "\t--run N  \tRun benchmarks N times"
-    echo -e "\t--check  \tCheck output for regressions"
-    echo -e "\t--clean  \tHousekeeping"
-    echo -e "\tSee script for details and filter selection."
+    echo -e "\t--compile   \tBuild all versions"
+    echo -e "\t--mod       \tBuild modified version"
+
+    echo -e "\t--run [N]   \tRun benchmarks N times"
+    echo -e "\t--coeffs [N]\tRun coefficient calculations N times"
+    echo -e "\t--check     \tCheck output for regressions"
+    echo -e "\t--clean     \tHousekeeping"
+    echo -e "\t\tSee script for further details and filter selection."
     exit 0
 fi 
 
 if [ "$1" == "--compile" ] ; then 
-mkdir $BINDIR
-for F in $FILTERS ; do 
+    mkdir $BINDIR
     echo "Compiling baseline $F"
     # Baseline
-    g++ -DBASELINE -DFILTER=$F -o $BINDIR/$F-base filterdriver.cpp   
+    g++ -DBASELINE -o $BINDIR/baseline filterdriver.cpp -O2
     # Baseline with compile-time optimization
-    g++ -DBASELINE -DFILTER=$F -o $BINDIR/$F-opt filterdriver.cpp -O3 -msse2 -ftree-vectorize -ffast-math
-done
+    g++ -DBASELINE -o $BINDIR/optimized filterdriver.cpp $OPTIMIZE
 fi
 
 if [ "$1" == "--compile" -o "$1" == "--mod" ] ; then 
-mkdir $BINDIR
-for F in $FILTERS ; do 
+    mkdir $BINDIR
     echo "Compiling modified $F"
-    # Modified version, optimized compile
-    g++ -DFILTER=$F -o $BINDIR/$F-mod filterdriver.cpp -O3 -msse2 -ftree-vectorize -ffast-math
-done
+    g++ -DFILTER=$F -o $BINDIR/modified filterdriver.cpp $OPTIMIZE
 fi
 
-if [ "$1" == "--run" ] ; then 
+if [ "$1" == "--run" -o "$1" == "--coeffs" ] ; then 
     for F in $FILTERS ; do
-	for SUFFIX in base opt mod ; do 
+	for BINARY in baseline optimized modified ; do 
+
 	    if [ $# -gt 1 ] ; then 
 		COUNT=$(($2+0))
 	    else
 		COUNT=1
 	    fi
+
+	    COEFFS=""
+	    if [ $1 == "--coeffs" ] ; then 
+		COEFFS="coeffs"
+	    fi
+
 	    while [ $((COUNT--)) -gt 0 ] ; do
-		/usr/bin/time -f "%U %C" $BINDIR/$F-$SUFFIX
+		/usr/bin/time -f "%U %C" $BINDIR/$BINARY $F
 	    done
 	    echo ""
 	done
@@ -58,11 +65,11 @@ if [ "$1" == "--run" ] ; then
 fi
 
 if [ "$1" == "--check" ] ; then 
-    # Compare output of optimized and modified
+    # Compare output of optimized and modified, doesn't work too well with -ffast-math
     mkdir $OUTDIR
     for F in $FILTERS ; do 
-	$BINDIR/$F-opt output > $OUTDIR/$F-opt
-	$BINDIR/$F-mod output > $OUTDIR/$F-mod
+	$BINDIR/optimized $FILTER output > $OUTDIR/$F-opt
+	$BINDIR/modified $FILTER output > $OUTDIR/$F-mod
 	if diff -sq $OUTDIR/$F-opt $OUTDIR/$F-mod >/dev/null ; then
 	    echo -e "OK    \t" $F
 	else
--- filterdriver.cpp
+++ filterdriver.cpp
@@ -7,44 +7,88 @@
 #include "basic_filters_modified.h"
 #endif
 
-#ifndef FILTER
-#error "No filter!"
-#endif
-
 #include <iostream>
-#include <string.h>
 
-// Adjust so one benchmark isn't too fast or too slow...
+using namespace std;
+
+// Adjust so any one benchmark isn't too fast or too slow...
 #define SAMPLERATE 96000
 #define ITERATIONS SAMPLERATE*100
 
-int main( int argc, const char* argv[] ) {
-
-  basicFilters<2> * filter = new basicFilters<2>( SAMPLERATE );
-  sample_t tmp1, tmp2;
-  int i;
-
+// Data to try, 600 frames is a realistic buffer size?
 #include "akwf_raw_005.h"
 
-  if( argc == 2 && strcmp(argv[1], "output")==0 ) {
-    filter->setFilterType(filter->FILTER);
-    filter->calcFilterCoeffs( 10000.0, 1.0 );
+int main( int argc, const char* argv[] ) {
 
-    // Assuming .25 seconds is enough to see any discrepancies
-    for(i=0; i<(SAMPLERATE>>2); i++) {
-      tmp1 = filter->update( buffer[i % BUFFERSIZE], 0 );
-      std::cout << tmp1 << std::endl;
-    }
-    return 0;
-  } else {
-    filter->setFilterType(filter->FILTER);
-    filter->calcFilterCoeffs( 10000.0, 1.0 );
+     const string filternames[] = { "LowPass", "HiPass", "BandPass_CSG", "BandPass_CZPG", "Notch",
+				    "AllPass", "Moog", "DoubleLowPass", "Lowpass_RC12", "Bandpass_RC12",
+				    "Highpass_RC12", "Lowpass_RC24", "Bandpass_RC24", "Highpass_RC24",
+				    "Formantfilter" };
+     const int filters=15;
 
-    for(i=0; i<ITERATIONS; i++) {
-      tmp1 = filter->update( buffer[i % BUFFERSIZE], 0 );
-      tmp2 = filter->update( buffer[(i+100) % BUFFERSIZE], 1 );
-    }
+     basicFilters<2> * filter = new basicFilters<2>( SAMPLERATE );
+     sample_t tmp1, tmp2;
+     int i;
 
-  }
-  return 0;
+     if( argc < 2 ) {
+	  cout << argv[0] << " filter [output|denormal]" << endl;
+	  cout << "Valid filters:" <<endl;
+	  for(i=0;i<filters; i++) {
+	       cout << filternames[i] << " ";
+	  }
+	  cout << endl;
+	  return 1;
+     }
+
+     int selection = -1;
+     for(i=0;i<filters; i++) {
+	  if(string(argv[1]) == filternames[i])
+	       selection = i;
+     }
+     if(i==-1) {
+	  cout << "Filter '" << argv[1] << "' invalid." << endl;
+	  return 2;
+     }
+
+     filter->setFilterType(selection);
+
+
+     if(argc>2 && string(argv[2]) == "output") {
+	  filter->calcFilterCoeffs( 10000.0, 1.0 );
+	  
+	  // Assuming .25 seconds is enough to see any discrepancies
+	  for(i=0; i<(SAMPLERATE>>2); i++) {
+	       tmp1 = filter->update( buffer[i % BUFFERSIZE], 0 );
+	       cout << tmp1 << endl;
+	  }
+	  return 0;
+     } 
+
+     // Tweak the buffer contents if needed
+     if(argc>2 && string(argv[2]) == "denormal") {
+	  for(i=0; i<BUFFERSIZE; i++) {
+	       buffer[i] = 1e-39;
+	  }
+     }
+
+     // Test only setting frequency
+     if(argc>2 && string(argv[2]) == "coeffs") {
+	  for(i=0; i<BUFFERSIZE; i++) {
+	       buffer[i] = 8000.0 + buffer[i]*7000.0;
+	  }
+	  for(i=0; i<ITERATIONS; i++) {
+	       filter->calcFilterCoeffs( buffer[i % BUFFERSIZE], (i%100)*.05);
+	  }
+     }
+
+     // Default benchmark: run through a lot of stereo samples
+     filter->calcFilterCoeffs( 10000.0, 1.0 );
+     
+     for(i=0; i<ITERATIONS; i++) {
+	  tmp1 = filter->update( buffer[i % BUFFERSIZE], 0 );
+	  tmp2 = filter->update( buffer[(i+100) % BUFFERSIZE], 1 );
+     }
+     
+
+     return 0;
 }
